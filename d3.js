1cd9dcb946efbc49a5ad00209bb6283ada1ab5b1
Ashwin <ash.ps312@gmail.com>
2019-02-15 15:07:15 -0800

    d3.js basics

---
+++ d3.js
@@ -0,0 +1,14 @@
+D3.js can be used in a html file in 2 ways:
+  1. Use a content delivery network url
+  2. Download the d3.js zip file and point to min.js file in html
+D3.js is ideal for:
+  1. Working with data manipulated DOM
+  2. Working with shapes
+  3. Creating interesting data visualization
+  and much more
+Understanding DOM, HTML, Javascript and SVG is crucial while learning D3.js
+  DOM is responsible for laying out the frame of any web page.
+  HTML is used to build every element in the web page.
+  CSS is used to make the elements look more polished.
+  Javascript works on the client side and operates on elements that are rendered.
+
